87a4aa55284a78f363ac69bdbdbc118f7fe75506
uselessconsumer <eliottdale@gmail.com>
2023-09-07 18:41:41 -0400

    make function to randomly get a point from a set coordinate

--- sketch.js
+++ sketch.js
@@ -4,12 +4,33 @@
 
 function setup() {
   createCanvas(400, 400);
+  console.log(Math.floor(random(50)));
 }
 
+//a function that will generate a point within a 25x25 pixel boundary.
+//create a 
+function randomPoint(x, y) {
+
+    strokeWeight(5);
+    square(x, y, 25);
+    stroke('green');
+    let pointX = x + Math.floor(random(25));
+    let pointY = y + Math.floor(random(25));
+    point(pointX, pointY)
+
+}
+
+
+
+
 function draw() {
+  noLoop();
   background(220);
-  strokeWeight(5);
-  stroke('green')
-  point(25, 25);
+
+  strokeWeight(1);
+  stroke('black')
+  square(100, 100, 25);
+
+  randomPoint(200, 200);
 
 }
