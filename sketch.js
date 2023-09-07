299d63b0a059e4f0cbc0ebbf050761e82e903e33
uselessconsumer <eliottdale@gmail.com>
2023-09-07 18:20:56 -0400

    setup project

---
+++ sketch.js
@@ -0,0 +1,15 @@
+//generate a star where each point has the chance to be drawn within a certain point, giving the star a hand drawn effect.
+
+//
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  strokeWeight(5);
+  stroke('green')
+  point(25, 25);
+
+}
