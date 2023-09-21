71a71010bd7492c15301f771f614b9cf9942d91e
uselessconsumer <eliottdale@gmail.com>
2023-09-21 17:10:10 -0400

    generate a funky looking star

--- sketch.js
+++ sketch.js
@@ -1,6 +1,3 @@
-//generate a star where each point has the chance to be drawn within a certain point, giving the star a hand drawn effect.
-
-//
 
 function setup() {
   createCanvas(400, 400);
@@ -33,15 +30,47 @@ function draw() {
   let coordinate = '200, 200';
   
 
-  let x1 = randomX(25);
+  let x1 = randomX(200);
   let y1 = randomX(25);
-  let x2 = randomX(100);
-  let y2 = randomX(25);
-  let x3 = randomX(100);
-  let y3 = randomX(50);
 
-  line(x1, y1, x3, y3);
+  let x2 = randomX(225);
+  let y2 = randomX(150);
+
+  let x3 = randomX(325);
+  let y3 = randomX(150);
+
+  let x4 = randomX(275);
+  let y4 = randomX(225);
+
+  let x5 = randomX(300);
+  let y5 = randomX(350);
+
+  let x6 = randomX(200);
+  let y6 = randomX(275);
+
+  let x7 = randomX(100);
+  let y7 = randomX(350);
+
+  let x8 = randomX(150);
+  let y8 = randomX(225);
+
+  let x9 = randomX(50);
+  let y9 = randomX(150);
+
+  let x10 = randomX(175);
+  let y10 = randomX(150);
+
+
+
   line(x1, y1, x2, y2);
   line(x2, y2, x3, y3);
-
+  line(x3, y3, x4, y4);
+  line(x4, y4, x5, y5);
+  line(x5, y5, x6, y6);
+  line(x6, y6, x7, y7);
+  line(x7, y7, x8, y8);
+  line(x8, y8, x9, y9);
+  line(x9, y9, x10, y10);
+  line(x10, y10, x1, y1);
+  
 }
